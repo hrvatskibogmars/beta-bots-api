e8a45b1ef40730b8ea8db7d50660eeaa13088401
Hrvoje <hrvoje.orejas@gmail.com>
2017-05-19 01:23:14 +0200

    fix post authentication:

--- betabots/config/common.py
+++ betabots/config/common.py
@@ -205,6 +205,7 @@ class Common(Configuration):
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
             'rest_framework.renderers.BrowsableAPIRenderer',
+
         ),
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.IsAuthenticated',
